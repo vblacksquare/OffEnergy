2682ea9e005b9ed2760c0845409b68fee2f77eec
vblacksquare <minka.denis@gmail.com>
2026-02-14 00:14:05 +0200

    fix deploy

--- scheduler/cheduler.py
+++ scheduler/cheduler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
 
 from .mykoaliv import update_mykolaiv
 from .change import update_pusher
@@ -27,7 +28,7 @@ scheduler.add_job(
 scheduler.add_job(
     id="update_state",
     func=update_state,
-    trigger=IntervalTrigger(seconds=30),
+    trigger=CronTrigger(minute='15,20', second=10),
     next_run_time=datetime.now(),
     misfire_grace_time=100
 )
